f03b14adefc738c4e5e614f2098c744ed0a23860
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-05-30 20:03:29 +0100

    Create example blockchain in main

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
+	"fmt"
 	"strconv"
 	"time"
 )
@@ -61,5 +62,18 @@ func NewBlockchain() *Blockchain {
 }
 
 func main() {
+	// Create a blockchain
+	myBlockchain := NewBlockchain()
 
+	// Add blocks to the new blockchain
+	myBlockchain.AddBlock("Send 1 ETH to Joseca")
+	myBlockchain.AddBlock("Send 2 ETH to Giove")
+
+	// Print attributes from each block
+	for _, block := range myBlockchain.blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevHash)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Println()
+	}
 }
