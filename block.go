9003cf8c93990ed3965bdb2c280705ab68274f2b
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-06-02 20:54:03 +0100

    Update creation of hash and add nonce to block

--- block.go
+++ block.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"strconv"
 	"time"
 )
 
@@ -13,26 +10,22 @@ type Block struct {
 	PrevHash  []byte
 	Hash      []byte
 	Data      []byte
+	Nonce     int
 }
 
-// Set the hash of a block
-func (b *Block) SetHash() {
-	// Convert timestamp from int64 to byte slice. First convert to string and then to []byte
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	// Join together Timestamp, PrevHash and Data to create a new header
-	header := bytes.Join([][]byte{timestamp, b.PrevHash, b.Data}, []byte{})
-	// Create a hash from the header. This creates an array of 32bytes
-	hash := sha256.Sum256(header)
-	// Put hash into the block, converting it before to a slice of bytes
-	b.Hash = hash[:]
-}
+// Now hash is set with ProofOfWork!!!
 
 // Create a new Block
 func NewBlock(data string, prevHash []byte) *Block {
 	// Create a pointer to a new Block with the data
-	block := &Block{time.Now().Unix(), prevHash, []byte{}, []byte(data)}
-	// Compute the hash with setHash()
-	block.SetHash()
+	block := &Block{time.Now().Unix(), prevHash, []byte{}, []byte(data), 0}
+	// Compute the hash and nonce with POW
+  pow := NewProofOfWork(block)
+  nonce, hash := pow.Run()
+
+  block.Nonce = nonce
+  block.Hash = hash[:]
+
 	// Return block
 	return block
 }
