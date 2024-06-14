862bd98adc889b079f241a76a311410cb06a5fca
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-06-14 14:13:43 +0100

    Create validateArgs and printUsage helper methods

--- cli.go
+++ cli.go
@@ -1,7 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 // CLI struct
 type CLI struct {
   bc *Blockchain
+}
+
+func (cli *CLI) printUsage() {
+  fmt.Println("How to use the program: ")
+  fmt.Println("  addblock -data BLOCK_DATA - add a block to the blockchain")
+	fmt.Println("  printchain - print all the blocks of the blockchain")
+}
+
+func (cli *CLI) validateArgs() {
+  // If there are less than two arguments ->
+  if len(os.Args) < 2 {
+    // Print the correct commands that user should use 
+    cli.printUsage()
+    // Close the program
+    os.Exit(1)
+  }
 }
 
